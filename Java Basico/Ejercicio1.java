89625eb6a071afb2a376967c8c1a4b403427cae7
Sebastian Mon <sebamon12@gmail.com>
2023-01-28 20:03:07 -0300

    Ejercicio 1 Java Basico

---
+++ Java Basico/Ejercicio1.java
@@ -0,0 +1,20 @@
+import org.w3c.dom.TypeInfo;
+
+public class Ejercicio1 {
+ 
+    public static void main(String[] args) {
+        int entero = 10;
+        long largo = 200L;
+
+        double doble = 40.2d;
+        boolean booleano = false;
+        String cadena = "Hola Open bootcamp";
+
+        System.out.println("Numero Entero "+ entero);
+        System.out.println("Numero Largo "+ largo);
+        System.out.println("Numero Doble "+ doble);
+        System.out.println("Booleano "+ booleano);
+        System.out.println("String "+ cadena);
+
+    }
+}
